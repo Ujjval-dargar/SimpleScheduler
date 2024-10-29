9c3478adbc37330042f9a40e38252d58f406d7b9
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2024-10-29 11:06:12 +0530

    adding global variables and input

---
+++ Codes/Simple_Shell.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "message.h"
+
+// command
+char *command = NULL;
+
+char *commands[100];
+int sz = 0;
+
+typedef struct
+{
+    char *command;
+    int pID;
+    struct timeval start;
+    struct timeval end;
+    bool submit;
+    bool schedule;
+} process;
+
+// stores last 100 previous command records
+process history[100];
+int sz_history = 0;
+
+int id;
+int fd;
+
+char cwd[1000];
+
+struct timeval sTime;
+struct timeval eTime;
+
+int ncpu, tslice;
+int schedulerPID;
+
+bool submit = false;
+bool shd = false;
+
+char *pipeName = "/tmp/Simple_Scheduler_pipe";
+
+
+// input and check if it's not empty
+bool input()
+{
+    command = (char *)malloc(100);
+    if (command == NULL)
+    {
+        printf("Failed to allocate memory for command.\n");
+        exit(0);
+    }
+
+    if (fgets(command, 100, stdin) == NULL)
+    {
+        printf("Failed to read input.\n");
+        exit(0);
+    }
+
+    strip(command);
+
+    return (strlen(command) != 0 && command[0] != '\n' && command[0] != ' ');
+}
+
+
+int main(const int argc, char const *argv[])
+{
+
+    return 0;
+}
